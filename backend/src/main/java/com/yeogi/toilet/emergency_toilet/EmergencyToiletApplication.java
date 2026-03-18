3ac10a982610fde3edc706fb3745f3460686d84c
LEE_SANG_JUN <sahwang04@gmail.com>
2026-03-18 22:08:19 +0900

    Preparing for a project

---
+++ backend/src/main/java/com/yeogi/toilet/emergency_toilet/EmergencyToiletApplication.java
@@ -0,0 +1,13 @@
+package com.yeogi.toilet.emergency_toilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyToiletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyToiletApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yeogi/toilet/emergency_toilet/EmergencyToiletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeogi.toilet.emergency_toilet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyToiletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
