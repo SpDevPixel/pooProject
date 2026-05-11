1664fb00112655b8f4879537bc3bbc729145c424
Sp_DevPixel <137591828+SpDevPixel@users.noreply.github.com>
2026-05-11 15:38:34 +0900

    admin 확인 절차

--- backend/src/main/java/com/yeogi/toilet/emergency_toilet/config/SecurityConfig.java
+++ backend/src/main/java/com/yeogi/toilet/emergency_toilet/config/SecurityConfig.java
@@ -1,29 +1,33 @@
 package com.yeogi.toilet.emergency_toilet.config;
 
+import com.yeogi.toilet.emergency_toilet.util.JwtAuthFilter;
+import com.yeogi.toilet.emergency_toilet.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/h2-console/**") // 클래스 없이 문자열로 바로 지정
-                )
-                .headers(headers -> headers
-                        .frameOptions(frameOptions -> frameOptions.sameOrigin())
-                )
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll() // 여기도 문자열로 지정
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
-                );
+                )
+                .addFilterBefore(new JwtAuthFilter(jwtUtil),
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
