7d427c1a4d5648e35ff45c5d66d68e1206212e74
LEE_SANG_JUN <sahwang04@gmail.com>
2026-06-10 19:28:06 +0900

    유저 강제탈퇴, 유저 정보 조회

--- backend/src/main/java/com/yeogi/toilet/emergency_toilet/admin/controller/AdminController.java
+++ backend/src/main/java/com/yeogi/toilet/emergency_toilet/admin/controller/AdminController.java
@@ -5,10 +5,9 @@ import com.yeogi.toilet.emergency_toilet.user.domain.User;
 import com.yeogi.toilet.emergency_toilet.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -19,10 +18,22 @@ public class AdminController {
     private final JwtUtil jwtUtil;
 
     @GetMapping("/user")
-    public ResponseEntity<User> getUserifm(@RequestHeader("Authorization") String token){
+    public ResponseEntity<List<User>> getAllUsers(@RequestHeader("Authorization") String token){
         Long id = jwtUtil.extractId(token.substring(7));
 
+        List<User> users = adminService.getAllUsers(id);
+        return ResponseEntity.ok(users);
     }
 
+    @DeleteMapping("/admin/user/{targetUserId}")
+    public ResponseEntity<String> forceWithdrawUser(
+            @RequestHeader("Authorization") String token,
+            @PathVariable("targetUserId") Long targetUserId // 강제 탈퇴시킬 유저의 ID
+    ){
+        Long id = jwtUtil.extractId(token.substring(7));
 
+        adminService.adminWithdrawUser(id,targetUserId);
+
+        return ResponseEntity.ok("회원이 성공적으로 강제 탈퇴 처리되었습니다.");
+    }
 }
